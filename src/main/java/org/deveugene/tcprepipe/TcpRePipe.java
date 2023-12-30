65e7387ab6c4df673119b28fdd3286c6e2c99078
Eugene <genyamateyuk@gmail.com>
2023-12-30 22:53:42 +0300

    init

---
+++ src/main/java/org/deveugene/tcprepipe/TcpRePipe.java
@@ -0,0 +1,7 @@
+package org.deveugene.tcprepipe;
+
+public class TcpRePipe {
+    public static void main(String[] args) {
+        throw new RuntimeException();
+    }
+}
---
+++ src/test/java/org/deveugene/tcprepipe/TcpRePipeTest.java
@@ -0,0 +1,9 @@
+package org.deveugene.tcprepipe;
+
+import org.junit.jupiter.api.Test;
+
+public class TcpRePipeTest {
+    @Test
+    public void testTrue() {
+    }
+}
